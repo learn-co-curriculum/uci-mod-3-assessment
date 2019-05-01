7de7401123bdcd69385fe483308687f485a0b574
Steven G. Harms <steven.harms@flatironschool.com>
2019-05-01 17:46:20 -0400

    sgh-scribbles

--- lib/oo_course_domain.rb
+++ lib/oo_course_domain.rb
@@ -1,75 +1,100 @@
+ require 'byebug'
+# Write your Student and Course classes here
+#
 class Student
-  attr_accessor :name, :course, :grade
-
   @@all = []
-
-  def initialize(name)
-    @name = name
-    @grade = nil
-    @@all << self
-  end
 
   def self.all
     @@all
+  end
+
+  def self.add_student(s)
+    @@all.push(s)
+  end
+
+  attr_reader :name
+  attr_accessor :course, :grade
+
+  def initialize(name)
+    @name = name
+    @course = nil
+    @grade = nil
+    Student.add_student(self)
+  end
+
+  def has_grade?
+    !@grade.nil?
+  end
+
+  def enroll_in_course(c)
+    @course = c
+  end
+
+  def setGrade(n)
+    @grade = n
   end
 end
 
 class Course
-  attr_accessor :name
-
   @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
 
   def self.all
     @@all
   end
 
-  def add_student(student)
-    student.course = self
-  end
-
-  def students
-    Student.all.select {|student| student.course == self}
-  end
-
-  def add_student_by_name(student_name_string)
-    student = Student.new(student_name_string)
-    student.course = self
+  def self.add_course(c)
+    @@all.push(c)
   end
 
   def self.student_count
-      Student.all.count
+    @@all.length
   end
 
-  def add_grade(student, grade)
-    student.grade = grade
+  attr_accessor :name
+  attr_reader :students
+
+  def initialize(name)
+    @name = name
+    @students = []
+    self.class.add_course(self)
+  end
+
+  def add_student(s)
+    s.enroll_in_course(self)
+    @students.push(s)
+  end
+
+  def add_student_by_name(name)
+    s = Student.new(name)
+    add_student(s)
+  end
+
+  def add_grade(s, gradeInt)
+    s.setGrade(gradeInt)
   end
 
   def all_existing_grades
-    self.students.select{|student| student.grade != nil }.map{|student| student.grade}
+    @students.select(&:has_grade?).map(&:grade)
   end
 
   def all_students_graded?
-    all_existing_grades.length > 0 && all_existing_grades.length == self.students.length
+    return false if @students.length == 0
+    return @students.all?(&:has_grade?)
   end
 
   def average_grade
     if all_students_graded?
-      (students.map{|student| student.grade }.inject{|sum, n| sum + n }) / ( students.length)
+      grades.reduce(:+) / @students.length
     else
-      'Grading still in progress.'
+       'Grading still in progress.'
     end
   end
 
-  def print_grades
-    students.each{|student| puts student.name + ": " + student.grade.to_s }
+  def grades
+    @students.map(&:grade)
   end
 
-  def self.student_count
-      Student.all.count
+  def print_grades
+    puts students.reduce(""){ |memo, s| memo += "#{s.name}: #{s.grade}\n" }
   end
 end
